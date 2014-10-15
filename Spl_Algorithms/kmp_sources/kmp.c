53fc36db04829af3c7261e1e3de83592e4b75eaf
Anil Kumar K K <akumarkk@outlook.com>
2014-10-15 02:41:10 -0600

    KMP String matching Algorithm

---
+++ Spl_Algorithms/kmp_sources/kmp.c
@@ -0,0 +1,51 @@
+#include <string.h> 
+#include <stdio.h>
+#include <stdlib.h>
+
+
+const char *kmp_search(const char *text, const char *pattern)
+{
+    int *T;
+    int i, j;
+    const char *result = NULL;
+ 
+    if (pattern[0] == '\0')
+        return text;
+ 
+    /* Construct the lookup table */
+    T = (int*) malloc((strlen(pattern)+1) * sizeof(int) );
+    T[0] = -1;
+    for (i=0; pattern[i] != '\0'; i++) {
+        T[i+1] = T[i] + 1;
+        while (T[i+1] > 0 && pattern[i] != pattern[T[i+1]-1])
+            T[i+1] = T[T[i+1]-1] + 1;
+    }
+ 
+    /* Perform the search */
+    for (i=j=0; text[i] != '\0'; ) {
+        if (j < 0 || text[i] == pattern[j]) {
+            ++i, ++j;
+            if (pattern[j] == '\0') {
+                result = text+i-j;
+                break;
+            }
+        }
+        else j = T[j];
+    }
+ 
+    free(T);
+    return result;
+}
+
+int
+main()
+{
+    char *ptr=NULL;
+    char text[10000]= "I am Anil. studying at University of U";
+    char pat[1024] = "Uni";
+
+
+    ptr = kmp_search(text, pat);
+    printf("found  %s\n", ptr);
+    return 0;
+}
