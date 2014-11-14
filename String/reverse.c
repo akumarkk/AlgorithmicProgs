b83a2f1c35ddec11e7da479230f0ab09e5322ae9
Anil Kumar K K <akumarkk@outlook.com>
2014-11-13 20:41:01 -0700

    String reversal problems

---
+++ String/reverse.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int
+main()
+{
+    char str[200] = "";
+    int  i,j;
+    printf("Enter String: ");
+    scanf("%s", str);
+
+    i=0;
+    j=strlen(str);
+
+    while(i < j)
+    {
+	str[j] = str[i];
+	str[i] = str[j-1];
+	i++; j--;
+    }
+
+    printf("Reversed String : %s\n", str);
+    return 0;
+}
